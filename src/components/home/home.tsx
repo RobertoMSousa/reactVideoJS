a431e16089a37d8008c08a4a6ee85c7ff2c73838
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-26 15:06:38 +0000

    add the home page and the teaser video

---
+++ src/components/home/home.tsx
@@ -0,0 +1,30 @@
+
+import * as React from 'react';
+
+// components
+import TopBar from '../topBar/topBar';
+import HomeVideo from '../homeVideo/homeVideo';
+import JoinUs from '../joinUs/joinUs';
+
+// css files
+import './home.css';
+
+// svg and images
+
+// class
+class Home extends React.Component <any, any> {
+
+	render() {
+		return (
+			<div>
+				<TopBar/>
+				<div className="homeMainWrapper">
+					<HomeVideo className="homeMainVideo"/>
+					<JoinUs className="homeMainJoin"/>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
---
+++ src/components/homeVideo/homeVideo.tsx
@@ -0,0 +1,55 @@
+
+import * as React from 'react';
+import * as videojs from 'video.js';
+
+// components
+
+// css files
+import './homeVideo.css';
+import 'video.js/dist/video-js.css';
+
+const videoJsOptions: videojs.PlayerOptions = {
+	autoplay: false,
+	controls: true,
+	preload: 'auto',
+	notSupportedMessage: 'please enable your JS to see the video',
+	sources: [{
+		src: 'https://vjs.zencdn.net/v/oceans.mp4',
+		type: 'video/mp4'
+	}]
+};
+
+let player: videojs.Player;
+let videoObj: any = undefined;
+
+// class
+class HomeVideo extends React.Component <any, any> {
+
+	constructor(props: any) {
+		super(props);
+		this.state = {
+		};
+	}
+
+	componentDidMount() {
+		// start the video player
+		player = videojs(videoObj, videoJsOptions,  function onPlayerReady() {
+			player.poster('http://vjs.zencdn.net/v/oceans.png');
+		});
+	}
+
+	// destroy player
+	componentWillUnmount() {
+		if (player) {
+			player.dispose();
+		}
+	}
+
+	render() {
+		return (
+			<video ref={node => videoObj = node} className="center homeVideo video-js"/>
+		);
+	}
+}
+
+export default HomeVideo;
