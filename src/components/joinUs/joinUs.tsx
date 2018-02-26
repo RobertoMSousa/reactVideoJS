37c95ffa62279bb4535830eeb731e7450b100038
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-26 15:07:21 +0000

    join us button

---
+++ src/components/joinUs/joinUs.tsx
@@ -0,0 +1,22 @@
+
+import * as React from 'react';
+
+// components
+
+// css files
+import './joinUs.css';
+// svg and images
+
+// class
+class JoinUs extends React.Component <any, any> {
+
+	render() {
+		return (
+			<div className="center joinUsButton">
+				<span className="joinUsText center">Join us for more!</span>
+			</div>
+		);
+	}
+}
+
+export default JoinUs;
